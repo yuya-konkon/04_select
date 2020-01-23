637fb234b083c341fe39ced703a9a260fe16ef59
yuya-konkon <yuuuuya20@icloud.com>
2020-01-23 22:40:15 +0900

    last commit

--- select.php
+++ select.php
@@ -20,5 +20,5 @@ $stmt->execute();
 $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($animals as $animal) {
-  echo $animal['type'] .  'の' . $animal['classifcation'] . '<br>' . $animal['description'] . '<br>' . $animal['birthday'] . '生まれ' . '<br>' . '出身地' . $animal['birthplace'] . '<hr>';
+  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>';
 }
