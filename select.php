e3b5e0d5e4d9f106670a6d7cf0cfc5d118b16c8f
yuya-konkon <yuuuuya08@icloud.com>
2020-01-30 09:14:24 +0900

    一回途中でコミット

--- select.php
+++ select.php
@@ -1,25 +1,3 @@
-<?php
-
-define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
-define('USER', 'staff');
-define('PASSWORD', '9999');
-
-try {
-  $dbh = new PDO(DSN, USER, PASSWORD);
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
-}
-
-$sql = "select * from animals";
-
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-
-$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -40,11 +18,36 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </form>
   </p>
 
-  <?php foreach ($animals as $animal) {
-    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '
-    <hr>';
-    } ?>
-
 </body>
 
 </html>
+<?php
+
+define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
+define('USER', 'staff');
+define('PASSWORD', '9999');
+
+try {
+  $dbh = new PDO(DSN, USER, PASSWORD);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+$sql = "select * from animals";
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $description = $_GET[''];
+} elseif ($title == '') {
+  foreach ($animals as $animal) {
+    echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '
+    <hr>';
+  }
+}
+
+?>
